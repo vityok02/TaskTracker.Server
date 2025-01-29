0bc4cfabe10324659476bc7d2ae953f4707c2f03
lipkan02 <lipkan02@gmail.com>
2025-01-29 21:04:50 +0200

    cleanup

--- Application/Projects/ProjectErrors.cs
+++
@@ -1,9 +0,0 @@
-﻿using Domain.Abstract;
-
-namespace Application.Projects;
-
-public static class ProjectErrors
-{
-    public static Func<string, Error> AlreadyExists => name 
-        => new("Project.AlreadyExists", $"Project with name {name} already exists.");
-}
